793e9bd4f614fa918e94a544dba06db57ddf0383
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-05 19:28:14 -0300

    style: aplica Code Maid em todo o projeto

--- DevCars.API/Controllers/CustomersController.cs
+++ DevCars.API/Controllers/CustomersController.cs
@@ -1,9 +1,5 @@
 ﻿using DevCars.API.InputModels;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DevCars.API.Controllers
 {
