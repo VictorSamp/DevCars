04883418b5cda99ac4ffd07d6e94d5b51a702cef
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-05 20:04:36 -0300

    refactor: corrige tipo errado do campo FullName

--- DevCars.API/Entities/Customer.cs
+++ DevCars.API/Entities/Customer.cs
@@ -5,7 +5,7 @@ namespace DevCars.API.Entities
 {
     public class Customer
     {
-        public Customer(int id, int fullName, string document, DateTime birthDate)
+        public Customer(int id, string fullName, string document, DateTime birthDate)
         {
             Id = id;
             FullName = fullName;
@@ -15,7 +15,7 @@ namespace DevCars.API.Entities
         }
 
         public int Id { get; private set; }
-        public int FullName { get; private set; }
+        public string FullName { get; private set; }
         public string Document { get; private set; }
         public DateTime BirthDate { get; private set; }
         public List<Order> Orders { get; private set; }
