1e4fc7d56ef265f107c023b78f56caea50a1b635
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-05 19:17:56 -0300

    refactor: adiciona modelos de entrada na lista de parametros dos metodos Post e PostOrder

--- DevCars.API/Controllers/CustomersController.cs
+++ DevCars.API/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevCars.API.InputModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,13 +11,13 @@ namespace DevCars.API.Controllers
     public class CostumersController : ControllerBase
     {
         [HttpPost]
-        public IActionResult Post()
+        public IActionResult Post([FromBody] AddCustomerInputModel model)
         {
             return Ok();
         }
 
         [HttpPost("{id}")]
-        public IActionResult PostOrder(int id)
+        public IActionResult PostOrder(int id, [FromBody] AddOrderInputModel model)
         {
             return Ok();
         }
