512da101afa9a367033b5a1703bdaec67bab315b
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-05 19:31:40 -0300

    feat: adiciona modelo de saida de pedidos

---
+++ DevCars.API/ViewModels/OderDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCars.API.ViewModels
+{
+    public class OderDetailsViewModel
+    {
+        public int IdCar { get; set; }
+        public int IdCostumer { get; set; }
+        public decimal TotalCost { get; set; }
+        public List<string> ExtraItems { get; set; }
+    }
+}
