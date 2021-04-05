6919cb114b8d8b57ebb9dc3392968402b44de0de
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-05 19:39:52 -0300

    feat: cria entidade Customer

---
+++ DevCars.API/Entities/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCars.API.Entities
+{
+    public class Customer
+    {
+        public Customer(int id, int fullName, string document, DateTime birthDate)
+        {
+            Id = id;
+            FullName = fullName;
+            Document = document;
+            BirthDate = birthDate;
+            Orders = new List<Order>();
+        }
+
+        public int Id { get; set; }
+        public int FullName { get; set; }
+        public string Document { get; set; }
+        public DateTime BirthDate { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}
