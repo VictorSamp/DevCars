f9671bbc716a6ee4b855bd9b67ad1782333feffb
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-05 19:20:15 -0300

    feat: adiciona classe modelo CarItemViewModel

---
+++ DevCars.API/ViewModels/CarItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCars.API.ViewModels
+{
+    public class CarItemViewModel
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+    }
+}
