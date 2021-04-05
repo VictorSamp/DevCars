ddf00dc492de2ca5f57f57e720727e72dc8a3160
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-05 17:47:31 -0300

    feat: adiciona controller de clientes com metodos genericos

---
+++ DevCars.API/Controllers/CustomersController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCars.API.Controllers
+{
+    [Route("api/customers")]
+    public class CostumersController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult PostOrder(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}/orders/{orderId}")]
+        public IActionResult GetOrder(int id, int orderId)
+        {
+            return Ok();
+        }
+    }
+}
