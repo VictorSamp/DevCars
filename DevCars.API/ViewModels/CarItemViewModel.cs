6ddbfe9aa54f315f72581a6a32ce76b0195acfc2
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-05 19:26:46 -0300

    refactor: adiciona campo id no modelo

--- DevCars.API/ViewModels/CarItemViewModel.cs
+++ DevCars.API/ViewModels/CarItemViewModel.cs
@@ -7,6 +7,7 @@ namespace DevCars.API.ViewModels
 {
     public class CarItemViewModel
     {
+        public int Id { get; set; }
         public string Brand { get; set; }
         public string Model { get; set; }
         public decimal Price { get; set; }
