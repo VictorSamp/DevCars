9ae5fdc648f58d397bab38f72ecbe1066bf7f267
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-06 09:42:53 -0300

    style: aplica CodeMaid em todo o projeto para limpeza de codigo

--- DevCars.API/Persistence/DevCarsDbContext.cs
+++ DevCars.API/Persistence/DevCarsDbContext.cs
@@ -1,10 +1,6 @@
 ﻿using DevCars.API.Configurations;
 using DevCars.API.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DevCars.API.Persistence
 {
