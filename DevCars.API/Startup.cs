60c54a8e7a273679669f733ba029e13ba7649677
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-06 08:22:42 -0300

    refactor: configura servico do entityframework na classe startup

--- DevCars.API/Startup.cs
+++ DevCars.API/Startup.cs
@@ -1,6 +1,7 @@
 using DevCars.API.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,7 +21,10 @@ namespace DevCars.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<DevCarsDbContext>();
+            var connectionString = Configuration.GetConnectionString("DevCarsCs");
+
+            services.AddDbContext<DevCarsDbContext>(
+                    options => options.UseSqlServer(connectionString));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
